ab26b20b8430243132e6c1c6ad698afca32f9787
pluto <blueabysm@gmail.com>
2013-04-26 20:50:31 +0800

    add new feature: display title and artist in `mode-line'

--- douban-fm.el
+++ douban-fm.el
@@ -1,9 +1,11 @@
+(require-package 'web)
 (require 'web)
 
 (defconst *douban-fm-process-name* "Douban FM")
 (defvar *url-douban-play-list* "http://douban.fm/j/mine/playlist?type=n&sid=&pt=0.0&channel=1000789&from=mainsite&r=add455f9e4")
 (defvar *douban-fm-buffer-name* nil)
 (defvar *douban-fm-modeline-name* "Douban FM")
+(defvar *douban-fm-modeline-format* mode-line-format)
 (defvar *douban-fm-player-path* "mplayer")
 
 (setq douban-play-list)
@@ -23,13 +25,19 @@
 ;; get the first song of the list
 (defun douban-next-song ()
   (let ((song (pop douban-play-list)))
-    (douban-refresh-play-list)
+    (if (>= 1 (length douban-play-list))
+        (douban-refresh-play-list))
     (if (not (eq song nil))
         song)))
 
 (defun douban-play-song (song)
   (if (get-process *douban-fm-process-name*)
       (delete-process *douban-fm-process-name*))
+  (setq mode-line-format
+        (concat
+         " [Now Playing:] " (decode-coding-string (cdr (assoc 'title song)) 'utf-8)
+         " [Artist:] " (decode-coding-string (cdr (assoc 'artist song)) 'utf-8)))
+  (message mode-line-format)
   (let ((song-url (cdr (assoc 'url song))))
     (start-process
      *douban-fm-process-name*
@@ -37,8 +45,10 @@
      *douban-fm-player-path*
      "-slave"
      "-quiet"
-     (aget (douban-next-song) 'url))
-    (set-process-sentinel (get-process *douban-fm-process-name*) 'douban-auto-next-song)))
+     (aget song 'url))
+    (douban-next-song)
+    (set-process-sentinel
+     (get-process *douban-fm-process-name*) 'douban-auto-next-song)))
 
 (defun douban-auto-next-song (process event)
   (if (string= event "finished\n")
@@ -48,13 +58,14 @@
   (interactive)
   (process-send-string *douban-fm-process-name* "pause\n"))
 
-(defun douban-fm-play-next ()
+(defun douban-fm-play ()
   (interactive)
   (douban-play-song (douban-next-song)))
 
 (defun douban-fm-stop ()
   (interactive)
 ;;  (process-send-string *douban-fm-process-name* "stop\n")
+  (setq mode-line-format *douban-fm-modeline-format*)
   (delete-process *douban-fm-process-name*))
 
 (provide 'douban-fm)
